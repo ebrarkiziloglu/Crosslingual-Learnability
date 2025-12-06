41d168d877e4e9f2ccf97731ff115176187373a4
Ebrar Kızıloğlu <ebrarkiziloglu@gmail.com>
2025-12-06 12:57:14 +0100

    add multi pretraining scripts for ESL

---
+++ pretraining_multi_deu_eng.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition lrz-hgx-h100-94x4
+#SBATCH --gres gpu:1
+#SBATCH --time 2-00:00:00
+#SBATCH --output %j.out
+
+# Load conda
+source ~/.bashrc
+
+# Activate your environment
+conda activate babylm-pretrain
+
+# Run your training
+python train.py --mode multi \
+  --l1 deu --l2 eng --l1_path BabyLM-community/babylm-deu --l2_path BabyLM-community/babylm-eng \
+  --tokenizer_path tokenizers/bb24.model --logging_steps 100000 --max_tokens 10000000 --out models/10M
